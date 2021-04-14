5210351db0297f8698cb2460391eeb50e223e962
LukeOBrien1 <74532838+LukeOBrien1@users.noreply.github.com>
2021-04-14 20:02:14 +0100

    First playable APK
    added all of the basic functionality to play the game

---
+++ Assets/Scripts/BrickManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickManager : MonoBehaviour
+{
+    public int columns, rows;
+    public float spacing;
+    public GameObject brickPrefab;
+
+    private List<GameObject> bricks = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RemoveBrick(BrickManager brick)
+    {
+        bricks.Remove(brick.gameObject);
+        
+        if (bricks.Count == 0)
+        {
+            ResetLevel();
+            //FindObjectOfType<Ball>().IncreaseSpeed();
+        }
+    }
+
+    public void ResetLevel()
+    {
+        foreach (GameObject brick in bricks)
+        {
+            Destroy(brick);
+        }
+        bricks.Clear();
+
+        for (int x = 0; x < columns; x++)
+        {
+            for(int y = 0; y < rows; y++)
+            {
+                Vector2 spawnPosition = (Vector2)transform.position + new Vector2(x * (brickPrefab.transform.localScale.x + spacing), 
+                                                                                 -y * (brickPrefab.transform.localScale.y + spacing));
+                GameObject brick = Instantiate(brickPrefab, spawnPosition, Quaternion.identity);
+                bricks.Add(brick);
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Paddle : MonoBehaviour
+{
+
+    public float speed = 5f;
+    private float input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * input * speed;
+    }
+}
